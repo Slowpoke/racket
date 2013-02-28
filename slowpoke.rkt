3a649dc3aacdc47d0ea8287f218305b242c6cb2e
Slowpoke <edeath32@gmail.com>
2013-02-28 22:57:43 +0400

    Update slowpoke.rkt

--- slowpoke.rkt
+++ slowpoke.rkt
@@ -1,99 +1,114 @@
 #lang racket
 
-;;
-;; cleanup parentheses
-(define-syntax bind-macro
-    (syntax-rules (to)
-        { [_ id to a]                    [define-syntax id (syntax-rules () ([_ x (... ...)] [a x (... ...)]))] }
-        { [_ id (args ...) to body    ]  [define-syntax id (syntax-rules () ([_ args ...   ] [body         ]))] }
-        { [_ id (args ...) to body ...]  [define-syntax id (syntax-rules () ([_ args ...   ] [body ...     ]))] }
-        { [_ id rules a1 to b1]                                                                [define-syntax id (syntax-rules rules (a1 b1))] }
-        { [_ id rules a1 to b1 a2 to b2]                                                       [define-syntax id (syntax-rules rules (a1 b1) (a2 b2))] }
-        { [_ id rules a1 to b1 a2 to b2 a3 to b3]                                              [define-syntax id (syntax-rules rules (a1 b1) (a2 b2) (a3 b3))] }
-        { [_ id rules a1 to b1 a2 to b2 a3 to b3 a4 to b4]                                     [define-syntax id (syntax-rules rules (a1 b1) (a2 b2) (a3 b3) (a4 b4))] }
-        { [_ id rules a1 to b1 a2 to b2 a3 to b3 a4 to b4 a5 to b5]                            [define-syntax id (syntax-rules rules (a1 b1) (a2 b2) (a3 b3) (a4 b4) (a5 b5))] }
-        { [_ id rules a1 to b1 a2 to b2 a3 to b3 a4 to b4 a5 to b5 a6 to b6]                   [define-syntax id (syntax-rules rules (a1 b1) (a2 b2) (a3 b3) (a4 b4) (a5 b5) (a6 b6))] }
-        { [_ id rules a1 to b1 a2 to b2 a3 to b3 a4 to b4 a5 to b5 a6 to b6 a7 to b7]          [define-syntax id (syntax-rules rules (a1 b1) (a2 b2) (a3 b3) (a4 b4) (a5 b5) (a6 b6) (a7 b7))] }
-        { [_ id rules a1 to b1 a2 to b2 a3 to b3 a4 to b4 a5 to b5 a6 to b6 a7 to b7 a8 to b8] [define-syntax id (syntax-rules rules (a1 b1) (a2 b2) (a3 b3) (a4 b4) (a5 b5) (a6 b6) (a7 b7) (a8 b8))] } ))
+(provide (all-defined-out))
 
-{ bind-macro bind (to with)
-    [_ id            with helper to body    ] to [define (id         ) helper  body     ]
-    [_ id            with helper to body ...] to [define (id         ) helper (body ...)]
-    [_ id (args ...) with helper to body    ] to [define (id args ...) helper  body     ]
-    [_ id (args ...) with helper to body ...] to [define (id args ...) helper (body ...)]
-    [_ id                        to body    ] to [define (id         )         body     ]
-    [_ id                        to body ...] to [define (id         )        (body ...)]
-    [_ id (args ...)             to body    ] to [define (id args ...)         body     ]
-    [_ id (args ...)             to body ...] to [define (id args ...)        (body ...)] }
+; ------- cleanup parentheses -------
 
-{ bind-macro fn (=>)
-    [_    => body]     to [lambda ()  body     ]
-    [_    => body ...] to [lambda () (body ...)]
-    [_ xs => body]     to [lambda xs  body     ]
-    [_ xs => body ...] to [lambda xs (body ...)] }
+(require (for-syntax syntax/parse))
+(require (for-syntax syntax/parse/experimental/template))
 
-{ bind-macro when (else)
-    [_ e1 a1 else d    ]                         to [if e1 a1  d]
-    [_ e1 a1 else d ...]                         to [if e1 a1 (d ...)]
-    [_ e1 a1 _ e2 a2 else d    ]                 to [cond (e1 a1) (e2 a2) (#t  d)]
-    [_ e1 a1 _ e2 a2 else d ...]                 to [cond (e1 a1) (e2 a2) (#t (d ...))]
-    [_ e1 a1 _ e2 a2 _ e3 a3 else d    ]         to [cond (e1 a1) (e2 a2) (e3 a3) (#t  d)]
-    [_ e1 a1 _ e2 a2 _ e3 a3 else d ...]         to [cond (e1 a1) (e2 a2) (e3 a3) (#t (d ...))]
-    [_ e1 a1 _ e2 a2 _ e3 a3 _ e4 a4 else d    ] to [cond (e1 a1) (e2 a2) (e3 a3) (e4 a4) (#t d)]
-    [_ e1 a1 _ e2 a2 _ e3 a3 _ e4 a4 else d ...] to [cond (e1 a1) (e2 a2) (e3 a3) (e4 a4) (#t (d ...))] }
+(begin-for-syntax
 
-{ bind-macro with (do and =)
-    [_ a1 = b1 do body ...]                                     to [letrec ([a1 b1]) (body ...)]
-    [_ a1 = b1 and a2 = b2 do body ...]                         to [letrec ([a1 b1] [a2 b2]) (body ...)]
-    [_ a1 = b1 and a2 = b2 and a3 = b3 do body ...]             to [letrec ([a1 b1] [a2 b2] [a3 b3]) (body ...)]
-    [_ a1 = b1 and a2 = b2 and a3 = b3 and a4 = b4 do body ...] to [letrec ([a1 b1] [a2 b2] [a3 b3] [a4 b4]) (body ...)] }
+    (define-syntax bind-class
+        (syntax-rules (to)
+            { [_ id p1]                [define-syntax-class          id                     (pattern p1)] }
+            { [_ id p1 to t1]          [define-syntax-class          id #:attributes (tmpl) (pattern p1 #:with tmpl #'t1)] }
+            { [_ id p1 to t1 p2 to t2] [define-syntax-class          id #:attributes (tmpl) (pattern p1 #:with tmpl #'t1) (pattern p2 #:with tmpl #'t2)] } ))
+    (define-syntax bind-splicing-class
+        (syntax-rules (to)
+            { [_ id p1]                [define-splicing-syntax-class id                     (pattern p1)] }
+            { [_ id p1 to t1]          [define-splicing-syntax-class id #:attributes (tmpl) (pattern p1 #:with tmpl #'t1)] } 
+            { [_ id p1 to t1 p2 to t2] [define-splicing-syntax-class id #:attributes (tmpl) (pattern p1 #:with tmpl #'t1) (pattern p2 #:with tmpl #'t2)] } ))
 
-{ bind-macro try (catch)
-    [_ body ... catch e1 a1]                   to [with-handlers ([e1 a1]) (body ...)]
-    [_ body ... catch e1 a1 e2 a2]             to [with-handlers ([e1 a1] [e2 a2]) (body ...)]
-    [_ body ... catch e1 a1 e2 a2 e3 a3]       to [with-handlers ([e1 a1] [e2 a2] [e3 a3]) (body ...)]
-    [_ body ... catch e1 a1 e2 a2 e3 a3 e4 a4] to [with-handlers ([e1 a1] [e2 a2] [e3 a3] [e4 a4]) (body ...)] }
+    { bind-class do   [~literal do  ] }
+    { bind-class to   [~literal to  ] }
+    { bind-class else [~literal else] }
+    { bind-class when [~literal when] }
+    { bind-class with [~literal with] }
 
-{ bind-macro ~ ()
-    [_ x op y    ] to [op x  y     ]
-    [_ x op y ...] to [op x (y ...)] }
+    { bind-class expr-to/with   [~and expr:expr (~not (~or _:to   _:with))] to expr }
+    { bind-class expr-do/with   [~and expr:expr (~not (~or _:do   _:with))] to expr }
+    { bind-class expr-when/else [~and expr:expr (~not (~or _:when _:else))] to expr }
 
-{ bind-macro :: to cons }
-{ bind-macro @ to append }
+    { bind-splicing-class bind-id
+        [~seq id:id                    ] to  id
+        [~seq id:id ((~seq arg:id ...))] to [id arg ...] }
 
+    { bind-splicing-class helper-body
+        [~seq _:with       body:expr-to/with      ] to  body.tmpl
+        [~seq _:with (~seq body:expr-to/with ...+)] to [body.tmpl ...] }
+
+    { bind-splicing-class when-body
+        [~seq _:when e:expr       body:expr-when/else      ] to [e  body.tmpl     ]
+        [~seq _:when e:expr (~seq body:expr-when/else ...+)] to [e (body.tmpl ...)] }
+    { bind-splicing-class when-body-last
+        [~seq _:when e:expr       body:expr      ] to [e  body]
+        [~seq _:when e:expr (~seq body:expr ...+)] to [e (body ...)] }
+
+    { bind-splicing-class with-body
+        [~seq _:with id:id (~literal =)       body:expr-do/with      ] to [id  body.tmpl     ]
+        [~seq _:with id:id (~literal =) (~seq body:expr-do/with ...+)] to [id (body.tmpl ...)] }
+
+    { bind-splicing-class body
+              body:expr      to  body
+        [~seq body:expr ...] to [body ...] }
+
+    { bind-splicing-class pat-to-tmpl
+        [~seq a:expr _:to b:expr] to [a (template b)] }
+
+)
+
+(define-syntax (bind-macro stx)
+    (syntax-parse stx
+        { [_ id:id _:to a:id]                                      [template (define-syntax id (syntax-rules () ([_ x (... ...)] [a x (... ...)])))] }
+        { [_ id:id (~seq macro:pat-to-tmpl ...+)]                  [template (define-syntax (id stx) (syntax-parse stx macro.tmpl ...)  )] }
+        { [_ id:id ((~seq arg:id ...)) _:to (~seq body:expr ...+)] [template (define-syntax id (syntax-rules () ([_ arg ...] [body ...])))] } ))
+
+{ bind-macro bind
+    [_ id:bind-id (~seq helper:helper-body ...) _:to body:body] to [define id.tmpl helper.tmpl ... body.tmpl] }
+
+{ bind-macro fn
+    [_ (~optional args:expr) (~literal =>) body:body] to [lambda (?? args ()) body.tmpl] }
+
+{ bind-macro when
+    [(~seq when:when-body ...+) _:else else:body]   to [cond when.tmpl ... (#t else.tmpl)]
+    [(~seq when:when-body ...) last:when-body-last] to [cond when.tmpl ... last.tmpl] }
+
+{ bind-macro with
+    [(~seq with:with-body ...+) _:do body:body] to [letrec (with.tmpl ...) body.tmpl] }
+
+{ bind-macro ~ 
+    [_ x:expr op:expr y:body] to [op x y.tmpl] }
+
+{ bind-macro  ::  to cons }
 { bind-macro head to car }
 { bind-macro tail to cdr }
+{ bind-macro mod  to remainder }
 
-{ bind-macro any? to ormap }
-{ bind-macro all? to andmap }
+; ------- tests -------
 
-{ bind-macro mod to remainder }
+{ bind-macro write-line
+    [_ format-string args ...] to [displayln (format format-string args ...)] }
 
-;;
-;; tests
-{ bind-macro write-line (format-string args ...) to
-   displayln (format format-string args ...) }
+{ bind-macro try
+    [_ body:expr ... (~literal catch) handler:body] to [with-handlers ([(fn (exn) => #t) handler.tmpl]) (body ...)] }
 
 { bind-macro test-val (id action answ) to
-        with pair   = (try ~ #f :: action
-                       catch [fn (ex) => #t]
-                             [fn (ex) => ~ #t :: ex])
-         and raised = [head pair]
-         and result = [tail pair]
-          do
-        when raised [write-line "~a FAIL: raised exception ~v instead of returning a result" id result]
-        when [equal? result answ] [write-line "~a SUCCESS" id]
-        else (write-line "~a FAIL: returned wrong result ~v" id result) }
+        with pair = try ~ #f :: action
+                    catch fn (exn) => ~ #t :: exn
+        with exn? = head pair
+        with rslt = tail pair
+          do when exn?               write-line "~a FAIL: raised exception ~v instead of returning a result" id rslt
+             when [equal? rslt answ] write-line "~a SUCCESS" id
+             else                    write-line "~a FAIL: returned wrong result ~v" id rslt }
 
 { bind-macro test-exn (id action pred) to
-        with pair   = (try ~ #f :: action
-                       catch [fn (ex) => #t]
-                             [fn (ex) => ~ #t :: ex] )
-         and raised = [head pair]
-         and result = [tail pair]
-          do
-        when [~ raised and pred result] [write-line "~a SUCCESS" id]
-        when raised [write-line "~a FAIL: raised wrong exception ~v" id result]
-        else (write-line "~a FAIL: returned ~v instead of raising an exception" id result) }
+        with pair = try ~ #f :: action
+                    catch fn (exn) => ~ #t :: exn
+        with exn? = head pair
+        with rslt = tail pair
+          do when [~ exn? and pred rslt] write-line "~a SUCCESS" id
+             when exn?                   write-line "~a FAIL: raised wrong exception ~v" id rslt
+             else                        write-line "~a FAIL: returned ~v instead of raising an exception" id rslt }
 
-{ bind-macro error-with-msg? (msg) to fn (ex) => ~ (exn:fail? ex) and equal? msg (exn-message ex) }
+{ bind-macro error-with-msg? (msg) to fn (exn) => ~ (exn:fail? exn) and equal? msg (exn-message exn) }
